925234cd72221d24e4311d26f3df312d082a3b0a
小椋寿希也 <jukiya@Juki-MacBookAir.local>
2021-02-20 18:43:17 +0900

    first commit

---
+++ src/wasurenbou.js
@@ -0,0 +1,235 @@
+//CHANNEL_ACCESS_TOKENを設定
+//LINE developerで登録をした、CHANNEL_ACCESS_TOKENを入力する
+const CHANNEL_ACCESS_TOKEN = "6fA9vZ5Q+RMVBhxDvIMdPioTPAQyx596gjbihezQOT8grTS2MI98dVcpUFEaevo4yATuqiLeNNa2ky9u0C96GeeYRV8Uaha7mac04Vz0AbKNYFSI0zIqvNvtachlomMIOnsYLoRHSFSSPcm6OKuUjAdB04t89/1O/w1cDnyilFU="; 
+const line_endpoint = "https://api.line.me/v2/bot/message/reply";
+const line_endpoint_push = "https://api.line.me/v2/bot/message/push";
+const line_endpoint_broadcast = "https://api.line.me/v2/bot/message/broadcast";
+
+const userId = "U1085335614a2d94b0536ad93b37aa8a5";
+
+//SpreadSheetの取得
+const SS = SpreadsheetApp.openById("1ZmhVDMe59nbHY8IxPTnkRaXGinrrPgl-A1gDK2Z1Msg"); //SpreadsheetのURL
+const sheet = SS.getSheetByName("買い物リスト"); //Spreadsheetのシート名（タブ名）
+const lastrow = sheet.getLastRow();
+const lastcol = sheet.getLastColumn();
+const sheetdata = sheet.getSheetValues(2, 1, lastrow, lastcol);
+
+//店舗問合せ
+const stores = {
+  sugi: "スギ薬局",
+  itoyokado: "イトーヨーカドー",
+  daiso: "100均",
+  supermarket: "スーパー",
+  list: "リスト",
+  other: "その他"
+}
+
+// 列番号
+const column = {
+  store: 1,
+  target: 2,
+  deadline: 3
+}
+
+// 毎日通知時間
+const alert = {
+  hour: 17,
+  minute: 30
+}
+
+// 毎日通知時間
+const alert_holiday = {
+  hour: 14,
+  minute: 30
+}
+
+// リスト追加の合言葉
+const addMassage = "追加";
+
+// 次の日の買い物リストに登録する時間
+const toNextDay = "1900";
+
+//POSTデータ取得、JSONをパースする
+function doPost(e) {
+  var json = JSON.parse(e.postData.contents);
+
+  //返信するためのトークン取得
+  var reply_token= json.events[0].replyToken;
+  if (typeof reply_token === 'undefined') {
+    return;
+  }
+
+  //送られたLINEメッセージを取得
+  let user_message = json.events[0].message.text;
+
+  // メッセージを分解
+  user_message = user_message.split(" ");
+
+  let todayList;
+  let reply_messages;
+  if (user_message[0] == addMassage) {
+    // 追加の場合
+    let result = addList(user_message);
+    if(result == "error") {
+      reply_messages = ["何言ってんだおめえ"];
+    } else {
+      reply_messages = [user_message[1] + "に" + user_message[2] + "を登録しました。"]
+    }
+  } else {
+    // 買い物リスト取得の場合
+    todayList = getTodayList(user_message);
+
+    // 返信する内容を作成
+    if (todayList == "") {
+      reply_messages = ["何言ってんだおめえ"];
+    } else {
+      console.log(todayList);
+      reply_messages = [todayList + "を買いなさいな。"];
+    }
+  }
+
+  // メッセージを返信（以下固定）
+  var messages = reply_messages.map(function (v) {
+    return {'type': 'text', 'text': v};    
+  });    
+  UrlFetchApp.fetch(line_endpoint, {
+    'headers': {
+      'Content-Type': 'application/json; charset=UTF-8',
+      'Authorization': 'Bearer ' + CHANNEL_ACCESS_TOKEN,
+    },
+    'method': 'post',
+    'payload': JSON.stringify({
+      'replyToken': reply_token,
+      'messages': messages,
+    }),
+  });
+  return ContentService.createTextOutput(JSON.stringify({'content': 'post ok'})).setMimeType(ContentService.MimeType.JSON);
+}
+
+// 買い物リストから今日のリストを出力する
+function getTodayList(user_message) {
+  var today = getToday();
+  var list = [];
+  var count = 0;
+  for (var i = 2; i < lastrow + 1; i++) {
+    let date = sheet.getRange(i, column.deadline).getValue();
+    let textStore = user_message[0];
+    let spreadStore = sheet.getRange(i, column.store).getValue();
+    console.log(date);
+    if (date == today && (textStore == stores.list || textStore == spreadStore)) {
+      list[count] = sheet.getRange(i, column.target).getValue() + " (" + sheet.getRange(i, column.store).getValue() + ")";
+      count += 1; 
+    }
+  }
+  console.log(list);
+
+  var sendList = "";
+  for (i = 0; i < list.length; i++) {
+    sendList = sendList + list[i] + "\n";
+  }
+  console.log(sendList);
+  return sendList;
+}
+
+// 買い物リストの追加
+function addList (user_message) {
+  if (user_message.length < 3 || user_message.length > 4) {
+    return "error";
+  }
+  sheet.getRange(lastrow + 1, column.store).setValue(user_message[1]);
+  sheet.getRange(lastrow + 1, column.target).setValue(user_message[2]);
+  if(user_message.length == 4) {
+    sheet.getRange(lastrow + 1, column.deadline).setValue(user_message[3]);
+  } else {
+    sheet.getRange(lastrow + 1, column.deadline).setValue(getToday());
+  }
+  return "success";
+}
+
+// 今日の日付を取得
+function getToday () {
+  let date = new Date();
+  let time = Utilities.formatDate( date, 'Asia/Tokyo', 'hhmm');
+  if (time > toNextDay) {
+    // 19時以降は次の日の買い物リストとして加算
+    date.setDate(date.getDate() + 1);
+  }
+  date = Utilities.formatDate( date, 'Asia/Tokyo', 'yyyyMMdd');
+  return date;
+}
+
+// 今日の買い物リストをお知らせ
+function alertTodayList () {
+  // まず使用済みのトリガーを削除
+  delTrigger();
+  Logger.log("トリガーを削除しました。");
+
+  // 買い物リスト取得の場合
+  todayList = getTodayList(["リスト"]);
+
+  // 返信する内容を作成
+  if (todayList == "") {
+    reply_messages = ["今日は買うものないよ"];
+  } else {
+    console.log(todayList);
+    reply_messages = ["今日は,\n" + todayList + "を買いなさいな。"];
+  }
+
+    // メッセージを返信（以下固定）
+  var messages = reply_messages.map(function (v) {
+    return {'type': 'text', 'text': v};    
+  });    
+  UrlFetchApp.fetch(line_endpoint_broadcast, {
+    'headers': {
+      'Content-Type': 'application/json; charset=UTF-8',
+      'Authorization': 'Bearer ' + CHANNEL_ACCESS_TOKEN,
+    },
+    'method': 'post',
+    'payload': JSON.stringify({
+      'messages': messages,
+    }),
+  });
+  return ContentService.createTextOutput(JSON.stringify({'content': 'post ok'})).setMimeType(ContentService.MimeType.JSON);
+}
+
+// トリガーを設定
+function setTrigger() {
+  const time = new Date();
+  console.log(time);
+    // 土日は12:30に通知
+  if (isHoliday) {
+    time.setHours(alert_holiday.hour);
+    time.setMinutes(alert_holiday.minute);    
+  } else {
+    // 平日は17:30に通知
+    time.setHours(alert.hour);
+    time.setMinutes(alert.minute);
+  }
+  
+  ScriptApp.newTrigger('alertTodayList').timeBased().at(time).create();
+}
+
+//　トリガーを削除
+function delTrigger() {
+  const triggers = ScriptApp.getProjectTriggers();
+  for (const trigger of triggers) {
+    if (trigger.getHandlerFunction() == "alertTodayList") {
+      ScriptApp.deleteTrigger(trigger);
+    }
+  }
+}
+
+// 休日を取得
+function isHoliday(date) {
+  date = new Date();
+  // 土日の判定
+  if (date.getDay() == 0 || date.getDay() == 6) return true;
+  // 祝日の判定
+  const id = 'ja.japanese#holiday@group.v.calendar.google.com'
+  const cal = CalendarApp.getCalendarById(id);
+  const events = cal.getEventsForDay(date);
+  //なんらかのイベントがある＝祝日
+  if (events.length) return true;
+
+  return false;
+}
