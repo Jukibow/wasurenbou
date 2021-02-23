bdf5900e33ad1d12d5c4adf4cf19a4a7f4990d0b
小椋寿希也 <jukiya@Juki-MacBookAir.local>
2021-02-23 19:13:05 +0900

    ミスを修正

--- src/wasurenbou.js
+++ src/wasurenbou.js
@@ -181,7 +181,7 @@ function addList (user_message) {
 // 買い物リストの追加（alexaから）
 function addListFromAlexa(data){
   let store = data.query_result.store;
-  let target = date.query_result.target;
+  let target = data.query_result.target;
   // スプレッドシートに追加
   sheet.getRange(lastrow + 1, column.store).setValue(store);
   sheet.getRange(lastrow + 1, column.target).setValue(target);
